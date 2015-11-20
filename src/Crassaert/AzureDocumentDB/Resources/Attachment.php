22aca0a72f01729f3a687b2a41152245002fab21
Cédric Rassaert <crassaert@wamiz.com>
2015-11-20 17:48:08 +0100

    Include attachment abilities

--- src/Crassaert/AzureDocumentDB/Resources/Attachment.php
+++ src/Crassaert/AzureDocumentDB/Resources/Attachment.php
@@ -1,7 +1,54 @@
 <?php
 /**
  * @Author: cedric
- * @Date:   2015-11-19 11:32:03
+ * @Date:   2015-11-19 11:30:35
  * @Last Modified by:   cedric
- * @Last Modified time: 2015-11-19 11:32:03
+ * @Last Modified time: 2015-11-20 17:30:15
  */
+namespace Crassaert\AzureDocumentDB\Resources;
+
+use Crassaert\AzureDocumentDB\Resources\Resources;
+
+class Attachment extends Resources {
+
+	protected $attachment;
+
+	public function createRawAttachment($attachment_name, $contentType, $slug, $select = true)
+	{
+		$headers = array('Slug: '.$slug, 'Content-Type: ' . $contentType)
+		$res = $this->azureDB->request->request(
+			'dbs/' . $this->azureDB->get('database')->getProperty('_rid') . '/colls/' . $this->azureDB->get('collection')->getProperty('_rid') . '/docs/' . $this->azureDB->get('document')->getProperty('_rid') . '/attachments',
+			'POST',
+			array('id' => $attachment_name),
+			'attachments',
+			$this->azureDB->get('document')->getProperty('_rid'),
+			$headers
+			);
+		
+		if ($select == true)
+		{
+			$this->attachment = $res;
+		}
+		
+		return $res;
+	}
+
+	public function createLinkAttachment($attachment_name, $contentType, $media, $select = true)
+	{
+		$res = $this->azureDB->request->request(
+			'dbs/' . $this->azureDB->get('database')->getProperty('_rid') . '/colls/' . $this->azureDB->get('collection')->getProperty('_rid') . '/docs/' . $this->azureDB->get('document')->getProperty('_rid') . '/attachments',
+			'POST',
+			array('id' => $attachment_name, 'contentType' => $contentType, 'media' => $media),
+			'attachments',
+			$this->azureDB->get('document')->getProperty('_rid')
+			);
+		
+		if ($select == true)
+		{
+			$this->attachment = $res;
+		}
+		
+		return $res;
+	}
+
+}
