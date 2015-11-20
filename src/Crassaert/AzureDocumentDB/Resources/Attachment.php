ddc88f67cc9cc384ce22b9ee08a5cd291206d5e4
Cédric Rassaert <crassaert@wamiz.com>
2015-11-20 17:52:41 +0100

    Fixing raw attachment

--- src/Crassaert/AzureDocumentDB/Resources/Attachment.php
+++ src/Crassaert/AzureDocumentDB/Resources/Attachment.php
@@ -3,7 +3,7 @@
  * @Author: cedric
  * @Date:   2015-11-19 11:30:35
  * @Last Modified by:   cedric
- * @Last Modified time: 2015-11-20 17:30:15
+ * @Last Modified time: 2015-11-20 17:52:32
  */
 namespace Crassaert\AzureDocumentDB\Resources;
 
@@ -13,13 +13,13 @@ class Attachment extends Resources {
 
 	protected $attachment;
 
-	public function createRawAttachment($attachment_name, $contentType, $slug, $select = true)
+	public function createRawAttachment($attachment_name, $contentType, $filePath, $slug, $select = true)
 	{
 		$headers = array('Slug: '.$slug, 'Content-Type: ' . $contentType)
 		$res = $this->azureDB->request->request(
 			'dbs/' . $this->azureDB->get('database')->getProperty('_rid') . '/colls/' . $this->azureDB->get('collection')->getProperty('_rid') . '/docs/' . $this->azureDB->get('document')->getProperty('_rid') . '/attachments',
 			'POST',
-			array('id' => $attachment_name),
+			file($filePath),
 			'attachments',
 			$this->azureDB->get('document')->getProperty('_rid'),
 			$headers
