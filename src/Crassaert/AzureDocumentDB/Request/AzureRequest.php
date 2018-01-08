2195fd62c1b4100cc8dc9b1454b781fab6f18391
Cédric Rassaert <crassaert@wamiz.com>
2018-01-08 16:29:20 +0100

    Adding CircleCI

--- src/Crassaert/AzureDocumentDB/Request/AzureRequest.php
+++ src/Crassaert/AzureDocumentDB/Request/AzureRequest.php
@@ -3,90 +3,84 @@
 # @Date:   2018-01-08T10:21:08+01:00
 # @Email:  crassaert@gmail.com
 # @Last modified by:   crassaert
-# @Last modified time: 2018-01-08T16:09:10+01:00
+# @Last modified time: 2018-01-08T16:22:37+01:00
 
 namespace Crassaert\AzureDocumentDB\Request;
 
-class AzureRequest {
+class AzureRequest
+{
+    protected $url;
+    protected $key;
+    protected $debug;
 
-	protected $url;
-	protected $key;
-	protected $debug;
+    public function __construct($url, $key, $debug = true)
+    {
+        $this->url = $url;
+        $this->key = $key;
+        $this->debug = $debug;
+    }
 
-	public function __construct($url, $key, $debug = true)
-	{
-		$this->url = $url;
-		$this->key = $key;
-		$this->debug = $debug;
-	}
+    public function request($path, $method = 'GET', $options = array(), $resource_type = null, $resource_id = '', $additional_headers = array())
+    {
+        $curl = curl_init($this->url . '/' . $path);
 
-	public function request($path, $method = 'GET', $options = array(), $resource_type = null, $resource_id = '', $additional_headers = array())
-	{
+        //curl_setopt($curl, CURLOPT_HEADER, 1);
+        curl_setopt($curl, CURLOPT_SSLVERSION, 1);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_VERBOSE, $this->debug);
+        curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
 
-		$curl = curl_init($this->url . '/' . $path);
+        if (!$resource_type) {
+            $resource_type = current(explode('/', $path));
+        }
 
-		//curl_setopt($curl, CURLOPT_HEADER, 1);
-		curl_setopt($curl, CURLOPT_SSLVERSION, 1);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_VERBOSE, $this->debug);
-		curl_setopt($curl, CURLOPT_CUSTOMREQUEST, $method);
+        $this->setRequestData($curl, $method, $resource_type, $resource_id, $options, $additional_headers);
 
-		if (!$resource_type)
-		{
-			$resource_type = current(explode('/', $path));
-		}
+        ($this->debug) ?
+        print "[[Debug: \nReq: ".curl_getinfo($curl, CURLINFO_HEADER_OUT) : $t=1;
 
-		$this->setRequestData($curl, $method, $resource_type, $resource_id, $options, $additional_headers);
+        //curl_setopt_array($curl, $options);
+        $result = curl_exec($curl);
+        $infos = curl_getinfo($curl);
 
-		($this->debug) ?
-		print "[[Debug: \nReq: ".curl_getinfo($curl, CURLINFO_HEADER_OUT) : $t=1;
+        curl_close($curl);
+        ($this->debug) ? print "\nRes: $result]]\n" : $t=1;
 
-		//curl_setopt_array($curl, $options);
-		$result = curl_exec($curl);
-		$infos = curl_getinfo($curl);
+        $result = json_decode($result);
+        $this->handleErrors($result, $infos);
 
-		curl_close($curl);
-		($this->debug) ? print "\nRes: $result]]\n" : $t=1;
+        return $result;
+    }
 
-		$result = json_decode($result);
-		$this->handleErrors($result, $infos);
+    protected function setRequestData(&$curl, $method, $resource_type, $resource_id, $options, $additional_headers)
+    {
+        $headers = array_merge($this->getAuthHeaders($method, $resource_type, $resource_id), $additional_headers);
 
-		return $result;
-	}
+        if ($method == 'POST' || $method == 'PUT') {
+            $data = json_encode($options);
 
-	protected function setRequestData(&$curl, $method, $resource_type, $resource_id, $options, $additional_headers)
-	{
-		$headers = array_merge($this->getAuthHeaders($method, $resource_type, $resource_id), $additional_headers);
-
-		if ($method == 'POST' || $method == 'PUT')
-		{
-			$data = json_encode($options);
-
-			if (isset($options['query']))
-			{
-				$headers[] = 'x-ms-documentdb-isquery: True';
+            if (isset($options['query'])) {
+                $headers[] = 'x-ms-documentdb-isquery: True';
                 $headers[] = 'x-ms-max-item-count: -1';
-				$headers[] = 'Content-Type: application/sql';
-				$headers[] = 'Content-Length: ' . strlen($options['query']);
+                $headers[] = 'Content-Type: application/sql';
+                $headers[] = 'Content-Length: ' . strlen($options['query']);
 
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $options['query']);
-			}
-			else
-			{
-				$headers[] = 'Content-Type: application/json';
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $options['query']);
+            } else {
+                $headers[] = 'Content-Type: application/json';
                 $headers[] = 'x-ms-max-item-count: -1';
-				$headers[] = 'Content-Length: ' . strlen($data);
+                $headers[] = 'Content-Length: ' . strlen($data);
 
-				curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
-			}
-		}
+                curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            }
+        }
 
-		$headers[] = 'api-key:' . $this->key;
-		curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
-	}
+        $headers[] = 'api-key:' . $this->key;
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+    }
 
 
-	/**
+    /**
    * getAuthHeaders
    *
    * @link http://msdn.microsoft.com/en-us/library/azure/dn783368.aspx
@@ -96,38 +90,37 @@ class AzureRequest {
    * @param string $resource_id   Resource ID
    * @return string Array of Request Headers
    */
-	protected function getAuthHeaders($verb, $resource_type, $resource_id)
-	{
-		$x_ms_date = gmdate('D, d M Y H:i:s T', strtotime('+2 minutes'));
-		$master = 'master';
-		$token = '1.0';
+    protected function getAuthHeaders($verb, $resource_type, $resource_id)
+    {
+        $x_ms_date = gmdate('D, d M Y H:i:s T', strtotime('+2 minutes'));
+        $master = 'master';
+        $token = '1.0';
 
-		$key = base64_decode($this->key);
-		$string_to_sign = $verb . "\n" .
-		$resource_type . "\n" .
-		$resource_id . "\n" .
-		$x_ms_date . "\n" .
-		"\n";
+        $key = base64_decode($this->key);
+        $string_to_sign = $verb . "\n" .
+        $resource_type . "\n" .
+        $resource_id . "\n" .
+        $x_ms_date . "\n" .
+        "\n";
 
-		$sig = base64_encode(hash_hmac('sha256', strtolower($string_to_sign), $key, true));
+        $sig = base64_encode(hash_hmac('sha256', strtolower($string_to_sign), $key, true));
 
-		return Array(
-			'Accept: application/json',
-			'User-Agent: cosmosdb.php.sdk/1.0.0',
-			'Cache-Control: no-cache',
-			'x-ms-date: ' . $x_ms_date,
-			'x-ms-version: 2017-02-22',
-			'authorization: ' . urlencode("type=$master&ver=$token&sig=$sig")
-			);
-	}
+        return array(
+            'Accept: application/json',
+            'User-Agent: cosmosdb.php.sdk/1.0.0',
+            'Cache-Control: no-cache',
+            'x-ms-date: ' . $x_ms_date,
+            'x-ms-version: 2017-02-22',
+            'authorization: ' . urlencode("type=$master&ver=$token&sig=$sig")
+            );
+    }
 
-	protected function handleErrors($response, $infos)
-	{
-		if ($infos['http_code'] >= 400)
-		{
-			var_dump($infos);
-			var_dump($response);
-			throw new \Exception($response->message, 1);
-		}
-	}
+    protected function handleErrors($response, $infos)
+    {
+        if ($infos['http_code'] >= 400) {
+            var_dump($infos);
+            var_dump($response);
+            throw new \Exception($response->message, 1);
+        }
+    }
 }
