bb6ce120d031e6b356942e28e5a7c8e28223f940
Cédric Rassaert <crassaert@wamiz.com>
2018-01-08 16:41:56 +0100

    Adding env for CI

--- test/OperationsTest.php
+++ test/OperationsTest.php
@@ -3,9 +3,8 @@
 # @Date:   2018-01-08T10:21:08+01:00
 # @Email:  crassaert@gmail.com
 # @Last modified by:   crassaert
-# @Last modified time: 2018-01-08T16:12:32+01:00
+# @Last modified time: 2018-01-08T16:41:24+01:00
 
-require dirname(__FILE__) . '/config.php';
 require dirname(__FILE__) . '/../vendor/autoload.php';
 
 use Crassaert\AzureDocumentDB\AzureDocumentDB;
@@ -13,14 +12,14 @@ use PHPUnit\Framework\TestCase;
 
 class OperationsTest extends TestCase
 {
-	const MAX_USERS = 10;
+    const MAX_USERS = 10;
 
     protected function getFakeUser()
     {
         $faker = Faker\Factory::create();
 
         $usr = [
-						'id'			=> $faker->md5,
+                        'id'			=> $faker->md5,
             'name'		=> $faker->name,
             'address' => $faker->address,
             'email'		=> $faker->email
@@ -31,6 +30,13 @@ class OperationsTest extends TestCase
 
     public function testDatabase()
     {
+        if (!isset($_ENV['AZURE_HOST']) && !isset($_ENV['AZURE_KEY'])) {
+            require dirname(__FILE__) . '/config.php';
+        } else {
+            define('AZURE_HOST', $_ENV['AZURE_HOST']);
+            define('AZURE_KEY', $_ENV['AZURE_KEY']);
+        }
+
         $db = new AzureDocumentDB(AZURE_HOST, AZURE_KEY, false);
         $db->get('database')->_list();
         $db->get('database')->create('cosmos_test');
@@ -45,7 +51,7 @@ class OperationsTest extends TestCase
         $db->get('collection')->_list();
         $users = $db->get('document')->_list();
 
-				$this->assertEquals(self::MAX_USERS, count($users->Documents));
+        $this->assertEquals(self::MAX_USERS, count($users->Documents));
 
         $db->get('document')->query('select * from user');
         $db->get('collection')->delete('user');
